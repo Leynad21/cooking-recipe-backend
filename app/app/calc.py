1f16c06f1be58aaf90062d0b502049ffb4319da1
Dany <dany21sayan_@hotmail.com>
2023-11-29 19:20:11 +0000

    wip: update dockerfiler

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+This is a sample module for testing
+"""
+
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return value"""
+    return x - y
